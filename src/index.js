76010ad95d51725b9f9cf9b15fe53dce83f04a02
Adriel Zarate <adriel.zarate@centricdigital.com>
2018-03-06 01:57:44 -0300

    webpack bootstrap

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import bootstrap from 'bootstrap';
+import css from './scss/style.scss';
+
+const url = 'http://localhost:3001/articles/';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('all done');
+});
---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'produccion';
+let scssLoaders = [];
+
+if (isProduction) {
+    scssLoaders = ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader?url=false&sourceMap=true', 'sass-loader?sourceMap=true']
+    });
+}
+else {
+    scssLoaders = [{
+        loader: 'style-loader',
+    }, {
+        loader: 'css-loader?url=false&sourceMap=true',
+    },{
+        loader: 'sass-loader?sourceMap=true'
+    }];
+}
+
+module.exports = {
+
+    entry: ['babel-polyfill', path.join(__dirname, 'src', 'index.js')],
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module : {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: scssLoaders
+            }, {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: path.join(__dirname, 'node_modules')
+            }, {
+                test: /assets\/.*/,
+                use: 'file-loader?name=[name].[ext]&useRelativePath=true'
+            }, {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    'file-loader?name=[name].[ext]&useRelativePath=true',
+                    'image-webpack-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/pages', 'index.html'),
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'article.html',
+            template: path.join(__dirname, 'src/pages', 'article.html'),
+            minify: {
+                collapseWhitespace: true
+            }
+        }),
+        new ExtractTextPlugin('style.css')
+    ],
+
+
+    devServer: {
+        open: true,
+        port: 3000,
+        overlay: true,
+        hot: true,
+        contentBase: [
+            path.join(__dirname, 'src/pagess'),
+            path.join(__dirname, 'src/pages/partials')
+        ],
+        watchContentBase: true
+    }
+};
+
