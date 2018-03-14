e2f8ab1b7b8ef8f325f68f3a925631e45854cb18
Adriel Zarate <adriel.zarate@centricdigital.com>
2018-03-14 13:49:10 -0300

    momentjs dates

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import solid from '@fortawesome/fontawesome-free-solid';
 import regular from '@fortawesome/fontawesome-free-regular';
 import css from './scss/style.scss';
 
+import moment from 'moment';
+import 'moment/locale/es';
 
-const url = 'http://localhost:3001/articles/';
+import { DateArticles } from './js/DateArticles';
+
+
+// const url = 'http://localhost:3001/articles/';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const dateArticles = new DateArticles(moment, '.article__date');
+    dateArticles.setTime();
+
     fontawesome.library.add(solid.faComment);
     fontawesome.library.add(regular.faHeart);
     fontawesome.library.add(solid.faHeartS);
---
+++ src/js/DateArticles.js
@@ -0,0 +1,48 @@
+export class DateArticles {
+
+    constructor(moment, selector) {
+        this.element = document.querySelectorAll(selector);
+        this.moment  = moment;
+        this.now     = this.moment(new Date());
+    }
+
+    getTime(el) {
+        const pubDate = el.getAttribute('datetime');
+        const dateDiff = this.moment.duration(this.now.diff(pubDate));
+
+        return {
+            seconds : dateDiff.asSeconds(),
+            minutes : dateDiff.asMinutes(),
+            hours   : dateDiff.asHours(),
+            days    : dateDiff.asDays(),
+            day     : this.moment(pubDate).format('dddd'),
+            date    : this.moment(pubDate).format('dddd d MMMM YYYY')
+        };
+    }
+
+    timeInArticle(times) {
+        if( times.seconds < 60 ) {
+            return 'Publicado hace ' + times.seconds + ' segundos';
+        }
+        if( times.minutes < 60 ) {
+            return 'Publicado hace ' + times.minutes + ' minutos';
+        }
+        if( times.hours < 24 ) {
+            return 'Publicado hace ' + times.hours + ' horas';
+        }
+        if( times.days < 7 ) {
+            return 'Publicado el ' + times.day;
+        }
+        return 'Publicado el ' + times.date;
+    }
+
+    setTime() {
+        const self = this;
+        [].forEach.call(this.element, function(element) {
+            const timeData = self.getTime(element);
+            element.innerHTML = self.timeInArticle(timeData);
+        });
+    }
+
+}
+
